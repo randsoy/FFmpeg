92e2682cb9cdd02f54bf47001f19fcba603284d1
befovy <befovy@gmail.com>
2022-01-16 15:38:08 +0800

    build: export symbols use in extern

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -1,6 +1,10 @@
 LIBAVCODEC_MAJOR {
     global:
         av*;
+        ff_h264_decode_extradata;
+        ff_h264_ps_uninit;
+        ff_hevc_decode_extradata;
+        ff_hevc_ps_uninit;
     local:
         *;
 };
--- libavutil/libavutil.v
+++ libavutil/libavutil.v
@@ -1,6 +1,10 @@
 LIBAVUTIL_MAJOR {
     global:
         av*;
+        add_dns_cache_entry;
+        remove_dns_cache_entry;
+        get_dns_cache_reference;
+        release_dns_cache_reference;
     local:
         *;
 };
