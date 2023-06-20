6c2fa9e1763c7b593edb8a5507c101a8f29cfb83
randsoy <randsoy68@gmail.com>
2023-06-20 15:05:21 +0800

    add ilenc protocol

--- libavformat/protocols.c
+++ libavformat/protocols.c
@@ -46,6 +46,7 @@ extern const URLProtocol ff_ijkmediadatasource_protocol;
 extern const URLProtocol ff_ijksegment_protocol;
 extern const URLProtocol ff_ijktcphook_protocol;
 extern const URLProtocol ff_ijkio_protocol;
+extern const URLProtocol ff_ilenc_protocol;
 extern const URLProtocol ff_mmsh_protocol;
 extern const URLProtocol ff_mmst_protocol;
 extern const URLProtocol ff_md5_protocol;
